eebb6960b59efc1f545cff79ee3cac81ae9b8127
Jack Fang <fang355@purdue.edu>
2025-07-03 19:53:22 -0400

    Make proxy clear

--- backend/server.js
+++ backend/server.js
@@ -69,7 +69,7 @@ app.use(session({ // extracts session ID from incoming cookie
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    // proxy: true,
+    proxy: true,
     cookie: {
         sameSite: 'None', // required for cross-site cookie sending
         secure: process.env.NODE_ENV === 'production' // must be true if SameSite=None
